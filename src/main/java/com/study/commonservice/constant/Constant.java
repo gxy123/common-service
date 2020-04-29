6aa509044de89f01e2537c9c5d3ffb800e3a51c1
GXY <ggg_xiaoyu@163.com>
2020-04-29 23:50:31 +0800

    常量

---
+++ src/main/java/com/study/commonservice/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.study.commonservice.constant;
+
+public class Constant {
+    public static String userId="userId";
+    public static String authorization="Authorization";
+    public static String tokenRedisKey ="jwt_redis_key";
+}
